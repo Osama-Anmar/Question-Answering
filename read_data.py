c22b8f17c799ba83dee1f6b12bbc0c99d4e874e9
Osama Anmar Sami <90341706+Osama-Anmar@users.noreply.github.com>
2024-08-28 15:40:52 +0300

    Delete read_data.py

--- read_data.py
+++
@@ -1,5 +0,0 @@
-def read_file(file_path, text_normalization):
-    file = open(file_path, encoding='utf-8-sig').read().lower().split("\n")
-    file =  [line.strip() for line in file if line.strip()] 
-    file =  list(map(text_normalization, file))
-    return file
