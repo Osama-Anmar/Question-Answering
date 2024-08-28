2ec20f89d52663d6be1a6407b3c043d094dcddb8
Osama Anmar Sami <90341706+Osama-Anmar@users.noreply.github.com>
2024-08-28 15:36:30 +0300

    Delete model_check_point.py

--- model_check_point.py
+++
@@ -1,8 +0,0 @@
-
-import tensorflow as tf
-def check_point(name):
-    return tf.keras.callbacks.ModelCheckpoint(
-        filepath = '{}_model_checkpoint.h5'.format(name),
-        monitor='loss',
-        save_best_only=True,
-        mode='min')
