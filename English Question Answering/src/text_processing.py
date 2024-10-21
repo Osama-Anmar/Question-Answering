3a428cac551ef6e07f70cb93f26ac2860476bb8d
Osama Anmar Sami <90341706+Osama-Anmar@users.noreply.github.com>
2024-10-21 19:47:41 +0300

    Add files via upload

--- English Question Answering/src/text_processing.py
+++ English Question Answering/src/text_processing.py
@@ -2,8 +2,8 @@ import numpy as np
 import re
 from itertools import chain
 
-class TextProcessing():
-    def data_words(self, data, filter = None):
+
+def data_words(data, filter = None):
         def filter_data(pattern):
             translator = str.maketrans('', '', pattern) 
             filtered_data = [word.translate(translator) for word in data]
@@ -19,19 +19,19 @@ class TextProcessing():
         return all_words + 1, words
     
 
-    def words_to_index_(self, words):
+def words_to_index_(words):
         words_to_index = {word:index+1 for index, word in enumerate(words)} 
         words_to_index['UNK'] = 0
         words_to_index = dict(sorted(words_to_index.items(), key=lambda item: item[1]))
         return words_to_index
     
-    def index_to_word_(self, words):
+def index_to_word_(words):
         index_to_word = {index+1:word for index, word in enumerate(words)}  
         index_to_word[0] = "UNK"
         index_to_word = dict(sorted(index_to_word.items(), key=lambda item: item[0]))
         return index_to_word
     
-    def word_counts(self, data):
+def word_counts(data):
         word_count = dict()
         for sentence in data:
             for word_ in sentence.split():
@@ -42,11 +42,11 @@ class TextProcessing():
         word_count = dict(sorted(word_count.items(), key=lambda item: item[1], reverse=True))
         return word_count
     
-    def text_to_sequence(self, word_index, data):
+def text_to_sequence(word_index, data):
      sequence_text = [[word_index[word] if word in word_index else  word_index["UNK"]  for word in sentences.split()] for sentences in data]
      return sequence_text
     
-    def sequences_padding(self, padding = 'post', input_sequence = None, max_length = None, truncating = 'post'):
+def sequences_padding(padding = 'post', input_sequence = None, max_length = None, truncating = 'post'):
         for i in range(0, len(input_sequence)):
            while len(input_sequence[i]) < max_length:
                 if padding == "post":
@@ -59,7 +59,7 @@ class TextProcessing():
                     input_sequence[i] =  input_sequence[i][:max_length]
         return  np.array(input_sequence)
     
-    def word_sequence_to_text(self, index_to_words, sequence):
+def word_sequence_to_text(index_to_words, sequence):
      sequence = list(sequence)   
      sequence = list(chain.from_iterable(sequence))
      word_sequence_to_text_ = [index_to_words[index] if index in index_to_words else index_to_words[0] for index in sequence]
